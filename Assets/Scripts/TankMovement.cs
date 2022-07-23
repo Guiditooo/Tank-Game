4419a458ce3f5a086901d9257812c6474d4adbdf
Guido Tello <lpmaleinn@gmail.com>
2022-07-23 01:00:20 -0300

    Created TankMovement Script

---
+++ Assets/Scripts/TankMovement.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+namespace ZapGames.TankGame
+{
+    public class TankMovement : MonoBehaviour
+    {
+        #region VARIABLES
+        #region SERIALIZED VARIABLES
+        [SerializeField] float maxSpeed;
+        [SerializeField] float rotationSpeed;
+        #endregion
+        
+        #region STATIC VARIABLES
+        #endregion
+        
+        #region PRIVATE VARIABLES
+        #endregion
+        #endregion
+        
+        #region METHODS
+        #region PUBLIC METHODS
+        #endregion
+        
+        #region STATIC METHODS
+        #endregion
+        
+        #region PRIVATE METHODS
+        #endregion
+        #endregion
+    }
+}
