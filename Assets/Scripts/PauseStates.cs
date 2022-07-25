1038c00c78e3531641f6d7475cc556409712a75c
Guido Tello <lpmaleinn@gmail.com>
2022-07-25 18:22:02 -0300

    Added Pause System Scripts

---
+++ Assets/Scripts/PauseStates.cs
@@ -0,0 +1,8 @@
+namespace ZapGames.TankGame
+{
+    public enum PauseStates
+    {
+        Paused,
+        Resumed
+    }
+}
---
+++ Assets/Scripts/PauseSystem.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using System;
+
+namespace ZapGames.TankGame
+{
+    public class PauseSystem : MonoBehaviour
+    {
+        public static Action<PauseStates> OnPauseStateChange;
+        public static bool Paused { get; private set; }
+        private void Awake()
+        {
+            InputManager.OnPausePress += PauseControl;
+        }
+
+        private void Start()
+        {
+            Paused = false;
+            PauseControl();
+        }
+
+        public static void PauseControl()
+        {
+            if (GameManager.GameRunning)
+            {
+                Paused = !Paused;
+                Time.timeScale = Paused ? 0 : 1;
+                PauseStates actualState = Paused ? PauseStates.Paused : PauseStates.Resumed;
+                Debug.Log("Envio estado de pausa: " + Paused);
+                OnPauseStateChange?.Invoke(actualState);
+            }
+        }
+        private void OnDestroy()
+        {
+            InputManager.OnPausePress -= PauseControl;
+        }
+    }
+}
