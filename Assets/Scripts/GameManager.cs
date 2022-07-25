a398f47e022bd06ef878aa8dfa432180adde6425
Guido Tello <lpmaleinn@gmail.com>
2022-07-25 18:22:39 -0300

    Added Game Manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+namespace ZapGames.TankGame
+{
+    public class GameManager : MonoBehaviour
+    {
+        #region VARIABLES
+        #region SERIALIZED VARIABLES
+
+        #endregion
+
+        #region STATIC VARIABLES
+        public static bool GameRunning { get; private set; } = true;
+        #endregion
+
+        #region PRIVATE VARIABLES
+
+        #endregion
+        #endregion
+
+        #region METHODS
+        #region PUBLIC METHODS
+
+        #endregion
+
+        #region STATIC METHODS
+
+        #endregion
+
+        #region PRIVATE METHODS
+
+        #endregion
+        #endregion
+    }
+}
