6b46d7a10631c8cd05c74970fd49bfbf39146091
Guido Tello <lpmaleinn@gmail.com>
2022-07-26 07:46:43 -0300

    Added Bouncer Script

---
+++ Assets/Scripts/Bouncer.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+namespace ZapGames.TankGame
+{
+    public class Bouncer : MonoBehaviour
+    {
+        #region VARIABLES
+        #region SERIALIZED VARIABLES
+        
+        #endregion
+        
+        #region STATIC VARIABLES
+        
+        #endregion
+        
+        #region PRIVATE VARIABLES
+        
+        #endregion
+        #endregion
+        
+        #region METHODS
+        #region PUBLIC METHODS
+        
+        #endregion
+        
+        #region STATIC METHODS
+        
+        #endregion
+        
+        #region PRIVATE METHODS
+        
+        #endregion
+        #endregion
+    }
+}
