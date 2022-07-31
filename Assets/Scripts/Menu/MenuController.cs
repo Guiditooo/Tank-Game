ca00b017e22fc940520d8ded134807808c3705b5
Guido Tello <lpmaleinn@gmail.com>
2022-07-31 04:15:02 -0300

    Improved Menu Controller Script
    Now the speed of showing/hiding can be pre-selected.

--- Assets/Scripts/Menu/MenuController.cs
+++ Assets/Scripts/Menu/MenuController.cs
@@ -1,81 +1,92 @@
+using UnityEngine.SceneManagement;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEngine.SceneManagement;
 
-public class MenuController : MonoBehaviour
+namespace ZapGames.TankGame
 {
-    [SerializeField] private CanvasGroup startingPanel;
-
-    private CanvasGroup actualPanel;
-
-    private void Awake()
+    public class MenuController : MonoBehaviour
     {
-        actualPanel = startingPanel;
+        [SerializeField] private CanvasGroup startingPanel;
+        [SerializeField] private float showSpeed = 1;
+        [SerializeField] private float hideSpeed = 1;
 
-        foreach (CanvasGroup canvasGroup in GetComponentsInChildren<CanvasGroup>())
+        private CanvasGroup actualPanel;
+
+        private void Awake()
         {
-            canvasGroup.alpha = 0;
-            canvasGroup.blocksRaycasts = false;
-            canvasGroup.interactable = false;
+            actualPanel = startingPanel;
+
+            foreach (CanvasGroup canvasGroup in GetComponentsInChildren<CanvasGroup>())
+            {
+                canvasGroup.alpha = 0;
+                canvasGroup.blocksRaycasts = false;
+                canvasGroup.interactable = false;
+            }
+
+            actualPanel.alpha = 1;
+            actualPanel.blocksRaycasts = true;
+            actualPanel.interactable = true;
+
         }
 
-        actualPanel.alpha = 1;
-        actualPanel.blocksRaycasts = true;
-        actualPanel.interactable = true;
-
-    }
-
-    private void Start()
-    {
-        Time.timeScale = 1;
-    }
-
-    public void StartPanel(CanvasGroup newPanel)
-    {
-        StartCoroutine(PanelChange(newPanel));
-    }
-
-    IEnumerator MakeItVisible(CanvasGroup panel)
-    {
-        float t = 0;
-        while (t<1)
+        private void Start()
         {
-            t += Time.deltaTime;
-            panel.alpha = t;
-            yield return null;
+            Time.timeScale = 1;
         }
-        panel.blocksRaycasts = true;
-        panel.interactable = true;
-    }
-    IEnumerator MakeItInvisible(CanvasGroup panel)
-    {
-        panel.blocksRaycasts = false;
-        panel.interactable = false;
-        float t = 1;
-        while (t > 0)
+
+        public void StartPanel(CanvasGroup newPanel)
         {
-            t -= Time.deltaTime;
-            panel.alpha = t;
-            yield return null;
+            StartCoroutine(PanelChange(newPanel));
         }
-    }
 
-    IEnumerator PanelChange(CanvasGroup panel)
-    {
-        yield return StartCoroutine(MakeItInvisible(actualPanel));
-        StartCoroutine(MakeItVisible(panel));
-        actualPanel = panel;
-    }
-    public void CloseGame()
-    {
-        Application.Quit();
-    }
+        IEnumerator MakeItVisible(CanvasGroup panel)
+        {
+            float t = 0;
+            while (t < 1)
+            {
+                t += Time.deltaTime * showSpeed;
+                panel.alpha = t;
+                yield return null;
+            }
+            panel.alpha = 1;
+            panel.blocksRaycasts = true;
+            panel.interactable = true;
+        }
+        IEnumerator MakeItInvisible(CanvasGroup panel)
+        {
+            panel.blocksRaycasts = false;
+            panel.interactable = false;
+            float t = 1;
+            while (t > 0)
+            {
+                t -= Time.deltaTime * hideSpeed;
+                panel.alpha = t;
+                yield return null;
+            }
+            panel.alpha = 0;
+        }
 
-    public void LoadGame()
-    {
-        SceneManager.LoadScene("Gameplay");
-    }
+        IEnumerator PanelChange(CanvasGroup panel)
+        {
+            yield return StartCoroutine(MakeItInvisible(actualPanel));
+            StartCoroutine(MakeItVisible(panel));
+            actualPanel = panel;
+        }
+        public void CloseGame()
+        {
+            Application.Quit();
+        }
 
+        public void LoadGame()
+        {
+            SceneManager.LoadScene("Gameplay");
+        }
+
+        public void ApplyChanges()
+        {
+
+        }
+
+    }
 }
