e006c8f32ea805e433fb181c8649e8b55a04aa4e
Guido Tello <lpmaleinn@gmail.com>
2022-08-01 05:58:39 -0300

    Updated Cannon Movement Script

--- Assets/Scripts/Gameplay/Tank/CannonMovement.cs
+++ Assets/Scripts/Gameplay/Tank/CannonMovement.cs
@@ -11,6 +11,7 @@ namespace ZapGames.TankGame
         [SerializeField] [Range(-4,0)] private float depressionAngle = -1;
         [SerializeField] [Range(0, 15)] private float elevationAngle = 10;
         [SerializeField] private float rotationSpeed;
+        [SerializeField] private Transform tankTransform;
         #endregion
 
         #region STATIC VARIABLES
@@ -20,6 +21,7 @@ namespace ZapGames.TankGame
         #region PRIVATE VARIABLES
         bool movingCannon = false;
         Vector3 actualTarget;
+        IEnumerator cannonMovementCoroutine = null;
         #endregion
 
         #region PUBLIC VARIABLES
@@ -47,53 +49,65 @@ namespace ZapGames.TankGame
         {
             TankController.MoveCannon -= AlignCannonWithTarget;
         }
-        private void AlignCannonWithTarget(Vector3 target)
+        private void AlignCannonWithTarget(Vector3 newTarget)
         {
 
-            if (movingCannon)
-            {
-                StopCoroutine(MoveCannon(actualTarget)); //Cancels actual movement
-                movingCannon = false; 
-            }
+            if(cannonMovementCoroutine!=null) StopCoroutine(MoveCannon(actualTarget)); //Cancels actual movement
 
-            StartCoroutine(MoveCannon(target)); //Starts a new corroutine with the new direction as target
-            actualTarget = target;
+            cannonMovementCoroutine = MoveCannon(newTarget);
+            StartCoroutine(cannonMovementCoroutine); //Starts a new corroutine with the new direction as target
+
+            actualTarget = newTarget;
 
         }
 
         IEnumerator MoveCannon(Vector3 target) //Without clamping depression and elevation angles
         {
+            Vector3 targetDirection = target - tankTransform.position;
+            Quaternion targetRotation = Quaternion.FromToRotation(transform.forward, targetDirection.normalized);
 
-            Quaternion targetRotation = Quaternion.FromToRotation(target,transform.position);
+            float targetAngle = Quaternion.Angle(transform.rotation, targetRotation);
+            //Debug.Log("Posicion objetivo> " + target + " - ANGLE WITH CANNON POS> " + targetAngle);
 
-            float t = 0;
+            float auxAngle = transform.rotation.eulerAngles.y;
 
-            movingCannon = true;
+            Debug.Log("Empieza en " + auxAngle + " Y debe llegar a " + targetAngle);
 
-            while(t<1)
+            while (auxAngle < targetAngle)
             {
-                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, t);
-                t += Time.deltaTime * rotationSpeed;
+                
+                transform.rotation = (target.x < tankTransform.position.x) ? Quaternion.Euler(0, -auxAngle, 0) : Quaternion.Euler(0, auxAngle, 0);
+                auxAngle += Time.deltaTime * rotationSpeed;
                 yield return null;
             }
+            transform.rotation = (target.x < tankTransform.position.x) ? Quaternion.Euler(0, -targetAngle, 0) : Quaternion.Euler(0, targetAngle, 0);
 
-            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 1);
 
-            movingCannon = false;
+            //Quaternion actualRotation = transform.rotation;
+            //float actualRotation = transform.rotation.y;
+            //while (t<1)
+            //{
+            //    transform.rotation = new Quaternion(transform.rotation.x, Mathf.Lerp(actualRotation, targetRotation.y, t), transform.rotation.z, transform.rotation.w);
+            //    //transform.rotation = Quaternion.Lerp(actualRotation, targetRotation, t);
+            //    t += Time.deltaTime * rotationSpeed;
+            //    yield return null;
+            //}
+            //transform.rotation = new Quaternion(transform.rotation.x, Mathf.Lerp(actualRotation, targetRotation.y, t), transform.rotation.z, transform.rotation.w);
+            //transform.rotation = Quaternion.Lerp(actualRotation, targetRotation, 1);
 
             TargetAligned?.Invoke(); // Once the cannon is aligned with the cannon, it is able to shoot.
 
         }
 
-        //IEnumerator MoveCannonY(Transform target) // Ill separate the movement of both X & Y axis.
-        //{
-        //    float t = 0;
-        //    while (t < 1)
-        //    {
-        //        t += Time.deltaTime * rotationSpeed;
-        //    }
-        //    yield return null;
-        //}
+        IEnumerator MoveCannonY(Transform target) // Ill separate the movement of both X & Y axis.
+        {
+            float t = 0;
+            while (t < 1)
+            {
+                t += Time.deltaTime * rotationSpeed;
+            }
+            yield return null;
+        }
 
         #endregion
         #endregion
